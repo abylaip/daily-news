5b6cbb10527d8a2190c074d6701c1106d8cab2a9
Abylay Aiyp <ablyaip@gmail.com>
2022-02-24 21:22:53 +0600

    Initial Commit

---
+++ daily-news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  daily-news
+//
+//  Created by gumball on 2/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
